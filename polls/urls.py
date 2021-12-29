ea21c790a2a77958a9a244408ab5ed3c517eee49
daramariabs <darasousa09@gmail.com>
2021-12-28 22:43:51 -0300

    Template renderizado

--- polls/urls.py
+++ polls/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    #polls/5/
+    path('<int:quetion_id>/', views.detail, name='detail'),
+    #polls/5/result
+    path('<int:quetion_id>/results/', views.results, name='results'),
+    #polls/5/vote
+    path('<int:quetion_id>/vote/', views.vote, name='vote'),
+    
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You are at the polls index.")
+    latest_quetion_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    context = {
+        'latest_quetion_list': latest_quetion_list
+    }
+    
+    return render(request, 'polls/index.html', context)
+
+def detail(request, quetion_id):
+    return HttpResponse("You're looking at question %s." % quetion_id)
+
+def results(request, quetion_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % quetion_id)
+
+def vote(request, quetion_id):
+    return HttpResponse("You're voting on question %s" % quetion_id)
