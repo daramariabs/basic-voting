c585f8237cb1dac3c2ae6fa808f4d60601983ac3
daramariabs <darasousa09@gmail.com>
2022-01-04 16:37:53 -0300

    Layout de administração alterado

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,8 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
+
 
 class Question(models.Model):
     quetion_text = models.CharField(max_length=200)
@@ -9,8 +11,12 @@ class Question(models.Model):
     def __str__(self):
         return self.quetion_text
 
-    # def was_published_recently(self):
-    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    @admin.display(
+        boolean = True,
+        ordering = 'pub_date',
+        description = 'Published recently?',
+    )
+
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
